5969b11536b30100590b77cb65a31a8d8b798772
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-08-17 22:30:36 -0300

    mapas

--- Aula09.dart
+++ Aula09.dart
@@ -1,1 +1,27 @@
-void main() {}
+void main() {
+  List<String> lista = ['Luan', 'Lena'];
+
+  Map<String, String> mapaNomes = {'nome': 'valor'};
+  print(lista[1]);
+  print(mapaNomes['nome']);
+
+  print("\n--------put---------\n");
+  mapaNomes.putIfAbsent('novaChave', () => 'novoValor');
+  print(mapaNomes);
+
+  print("\n--------Atribuição direta---------\n");
+  mapaNomes['novaChaveDois'] = 'NovoValorDois';
+  print(mapaNomes);
+
+  print("\n--------remove---------\n");
+  mapaNomes.remove('nome');
+  print(mapaNomes);
+
+  print("\n--------update---------\n");
+  mapaNomes.update('novaChaveDois', (value) => 'AtualizadoDois');
+  print(mapaNomes);
+
+  print("\n--------forEach---------\n");
+  mapaNomes.forEach((key, value) => print('a chave é $key, o valor é $value'));
+  print(mapaNomes);
+}
